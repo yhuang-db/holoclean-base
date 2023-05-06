7d43f0db856f30323e6f3c6a7023f91d86ebf280
Yuchuan Huang <itisyuchuan@gmail.com>
2023-05-06 11:33:53 -0500

    add one click run experiment

---
+++ run_exp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bash 51x_reset_pg_db_user.sh
+
+echo "DONE reset pg."
+
+bash init_venv.sh
+
+echo "DONE init venv."
+
+source venv/bin/activate
+
+echo "DONE activate venv."
+
+cd 0_exp
+
+echo "START exp."
+
+bash start_exp.sh hc_nyc_crash.py
