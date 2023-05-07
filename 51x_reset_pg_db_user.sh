3e42ae04df8cd2786169214578fe445008528521
Yuchuan Huang <itisyuchuan@gmail.com>
2023-05-07 16:24:46 -0500

    rename eqw exp db name

--- 51x_reset_pg_db_user.sh
+++ 51x_reset_pg_db_user.sh
@@ -4,7 +4,7 @@ sudo -u huan1531 psql -d postgres <<PGSCRIPT
 DROP DATABASE IF EXISTS holodb;
 DROP DATABASE IF EXISTS holodb_hc;
 DROP DATABASE IF EXISTS holodb_sparcle;
-DROP DATABASE IF EXISTS holodb_eqweight;
+DROP DATABASE IF EXISTS holodb_eqw;
 DROP USER IF EXISTS holouser;
 CREATE DATABASE holodb;
 CREATE USER holouser;
@@ -38,10 +38,10 @@ echo "holodb_sparcle and user has been created/reset."
 
 
 sudo -u huan1531 psql -d postgres <<PGSCRIPT
-CREATE DATABASE holodb_eqweight;
-GRANT ALL PRIVILEGES ON DATABASE holodb_eqweight TO holouser ;
-\c holodb_eqweight
+CREATE DATABASE holodb_eqw;
+GRANT ALL PRIVILEGES ON DATABASE holodb_eqw TO holouser ;
+\c holodb_eqw
 CREATE EXTENSION postgis;
 PGSCRIPT
 
-echo "holodb_eqweight and user has been created/reset."
+echo "holodb_eqw and user has been created/reset."
